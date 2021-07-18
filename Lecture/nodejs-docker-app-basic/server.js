ffc0017d15d18bd4b55a9e67125ef67e657a8863
jiho <jiho@kakao.com>
2021-07-18 18:49:10 +0900

    [DOCKER-35] Create server

---
+++ Lecture/nodejs-docker-app-basic/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 8080;
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+// Run
+app.listen(PORT);
