dd2d9caf289fe2d6f0435ceaeb4d51eb41630ca9
jiho <jiho@kakao.com>
2021-07-25 14:51:28 +0900

    [DOCKER-43] Create resdis-node app

---
+++ Lecture/nodejs-docker-compose-app/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const redis = require("redis");
+
+// Redis client
+const client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+
+const app = express();
+
+// 숫자는 0부터 시작
+client.set("number", 0);
+
+app.get('/', (req, res) => {
+    client("number", (err, number) => {
+        // 현재 숫자를 가져온 후 1씩 증가
+        client.set("number", parseInt(number) + 1);
+        res.send("숫자가 1씩 증가한다. 숫자: " + number);
+    })
+})
+
+app.listen(8080);
+console.log("server is running...");
